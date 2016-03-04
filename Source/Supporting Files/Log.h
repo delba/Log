c49a07f390e7c7417f0d559f3388c0c9fbd3d342
delba <delba@users.noreply.github.com>
2016-03-04 18:36:00 +0100

    Add OSX, watchOS and tvOS targets

--- Source/Supporting Files/Log.h
+++ Source/Supporting Files/Log.h
@@ -22,14 +22,7 @@
 // SOFTWARE.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
-//! Project version number for Log.
 FOUNDATION_EXPORT double LogVersionNumber;
-
-//! Project version string for Log.
 FOUNDATION_EXPORT const unsigned char LogVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Log/PublicHeader.h>
-
-
