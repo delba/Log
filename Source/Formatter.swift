b94fc49ec7a72df666e3abf1711928147fdf8398
delba <delba@users.noreply.github.com>
2016-05-07 22:02:59 +0200

    Add Utilities.swift

--- Source/Formatter.swift
+++ Source/Formatter.swift
@@ -313,26 +313,3 @@ private extension Formatter {
         return String(format: "%.3f%%", relativeStandardDeviation)
     }
 }
-
-internal extension String {
-    /// The last path component of the receiver.
-    var lastPathComponent: String {
-        return NSString(string: self).lastPathComponent
-    }
-    
-    /// A new string made by deleting the extension from the receiver.
-    var stringByDeletingPathExtension: String {
-        return NSString(string: self).stringByDeletingPathExtension
-    }
-    
-    /**
-     Returns a string colored with the specified color.
-     
-     - parameter color: The string representation of the color.
-     
-     - returns: A string colored with the specified color.
-     */
-    func withColor(color: String) -> String {
-        return "\u{001b}[fg\(color);\(self)\u{001b}[;"
-    }
-}
